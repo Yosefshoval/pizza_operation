753facf62cc93286d56661efe5c605ff584d3054
Yosef shoval <ys0542522504@gmail.com>
2026-02-16 12:17:23 +0200

    pipline of get_order enpoint

--- api/main.py
+++ api/main.py
@@ -1,12 +1,10 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, HTTPException
 from pydantic import ValidationError
 from schema import OrderRequest
 from mongo_connection import DBConnection
 from kafka_producer import publish_message
 from redis_producer import *
 import json
-
-
 
 mongo_client = DBConnection()
 app = FastAPI()
@@ -22,6 +20,7 @@ def post_orders(file: UploadFile):
             print(err)
             continue
         # 1: save in mongodb
+        order['status'] = 'PREPARING'
         mongo_client.save_order(order)
         # 2: publish to kafka
         publish_message(order)
@@ -32,10 +31,15 @@ def post_orders(file: UploadFile):
 
 
 
-
 @app.get('/order/{order_id}')
 def get_order(order_id: str):
-    pass
+    try:
+        order = get_order(order_id=order_id)
+        if order is None:
+            order = mongo_client.get_order_by_id(order_id)
+        return order
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 if __name__ == "__main__":
--- api/redis_producer.py
+++ api/redis_producer.py
@@ -1,8 +1,10 @@
 from os import getenv
-
+from redis import Redis
 
 REDIS_HOST = getenv('REDIS_HOST', 'localhost')
 REDIS_PORT = getenv('REDIS_PORT', 6441)
+
+r = Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
 
 
 def cache_order(order: dict):
